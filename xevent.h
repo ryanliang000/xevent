c436189529c1973a7455d951b3343c0cffb67cab
ryanliang000 <mail>
2021-02-10 22:45:23 +0800

    add xevent for epoll

---
+++ xevent.h
@@ -0,0 +1,224 @@
+#define MAX_EVENT_POOL 2048
+#define MAX_EVENT_RECV 128
+int _fdnums = 0;
+int _epfd = 0;
+enum xevent_filter{
+	xfilter_read = 0,
+	xfilter_write= 1,
+	xfilter_error= 2,
+	xfilter_count= 3
+};
+enum xevent_action{
+	xaction_add = 0,
+	xaction_del = 1,
+	xaction_count= 2
+};
+typedef int (*xevent_callback)(int fd, int filter);
+struct xevent_func{
+	int filter;
+	xevent_callback func;
+	xevent_func():filter(-1), func(NULL){}
+	void reset(){filter=-1; func=NULL;}
+};
+struct xevent{
+	int fd;
+	xevent_func funcs[xfilter_count];
+	xevent():fd(-1){}
+	xevent(int infd):fd(infd){}
+	bool valid(){
+		if (fd == -1) return false;
+		for (int i=0; i<xfilter_count; i++){
+			if (funcs[i].filter != -1)
+				return true;
+		}
+		return false;
+	}
+};
+struct xevent _xeventpool[MAX_EVENT_POOL];
+xevent* xeventpool(){
+	return _xeventpool;
+}
+const char* xfilterdesc(int xfilter){
+    static char _desc[64];
+    switch(xfilter){
+    case xfilter_read:
+        return "READ";
+    case xfilter_write:
+        return "WRITE";
+    case xfilter_error:
+        return "ERROR";
+    default:
+        sprintf(_desc, "flt-%d", xfilter);
+    }
+    return _desc;
+}
+// init xevent data
+int initxevent();
+// reg event callback func
+int regxevent(int fd, xevent_filter xfilter, xevent_callback func);
+// unreg event callback
+int unregxevent(int fd, xevent_filter xfilter);
+int unregxevent(int fd);
+// dispatch event once
+int dispatchxevent(int timeoutsecond);
+
+#include <sys/epoll.h>
+int xfilter2kfilter(int xfilter)
+{
+	switch(xfilter){
+	case xfilter_read:
+		return EPOLLIN;
+	case xfilter_write:
+		return EPOLLOUT;
+	case xfilter_error:
+		return EPOLLERR;
+	default:
+		;
+	}
+	return EPOLLERR;
+}
+xevent_filter kfilter2xfilter(int kfilter){
+	switch(kfilter){
+	case EPOLLIN:
+		return xfilter_read;
+	case EPOLLOUT:
+		return xfilter_write;
+	case EPOLLERR:
+		return xfilter_error;
+	default:
+		;
+	}
+	return xfilter_error;
+}
+// build epoll event 
+epoll_event buildkevent(xevent& evt)
+{
+    epoll_event epollevt;
+	epollevt.data.fd = evt.fd;
+	epollevt.events = 0;
+	if (evt.fd == -1) return evpollevt;
+    for (int i=0; i<xfilter_count; i++){
+		if (evt.funcs[i].filter != -1){
+			 epollevt.events|= xfilter2kfilter(evt.funcs[i].filter); 
+		}
+	}
+	return epollevt;
+}
+int initxevent() {
+	_epfd = epoll_create(MAX_EVENT_POOL);
+	if (_epfd == -1){
+		err_sys("init xevent with epoll error");
+	}
+}
+
+// 0-Succ, -1-failed
+int regxevent(int fd, xevent_filter filter, xevent_callback func)
+{
+	if (fd >= MAX_EVENT_POOL){
+		LOG_E("fd exceed max event pool size(%d >= %d)", fd, MAX_EVENT_POOL);
+		return -1;
+	}
+	xevent& evt = xeventpool()[fd];
+	// only update func
+	if (evt.funcs[filter].filter != -1){
+		evt.funcs[filter].func = func;
+		return 0;
+	}
+	// modify data
+	bool valid = evt.valid();
+	evt.fd = fd;
+	evt.funcs[filter].filter = filter;
+	evt.funcs[filter].func = func;
+	// add event
+	epoll_event epollevt = buildkevent(evt);
+	if (valid)
+		epoll_ctl(_epfd, EPOLL_CTL_MOD, fd, &epollevt);	
+	else
+		epoll_ctl(_epfd, EPOLL_CTL_ADD, fd, &epollevt);	
+	LOG_D("regevent: fd-%d, filter-%s", fd, xfilterdesc(filter));
+	_fdnums++;
+	return 0;
+};
+int unregxevent(int fd, xevent_filter filter)
+{
+	xevent& evt = xeventpool()[fd];
+	if (evt.fd == -1 || evt.funcs[filter].filter == -1){
+		LOG_D("remove fd-filter(%d-%d) event not exist", fd, filter);
+		return 0;
+	}
+	// remove data
+	evt.funcs[filter].reset();
+	// remove event
+    bool valid = evt.valid();
+	epoll_event epollevt = buildkevent(evt);
+	if (valid){
+        epoll_ctl(_epfd, EPOLL_CTL_MOD, fd, &epollevt);
+    }
+	else{
+        epoll_ctl(_epfd, EPOLL_CTL_DEL, fd, &epollevt);
+	}
+	LOG_D("unregevent: fd-%d, filter-%s", fd, xfilterdesc(filter));
+	_fdnums--;
+	return 0;
+};
+int unregxevent(int fd)
+{
+	xevent& evt = xeventpool()[fd];
+	if (evt.fd == -1){
+		LOG_D("remove fd(%d) event not exist", fd);
+		return 0;
+	}
+	struct kevent kevts[xfilter_count];
+	int nums = 0;
+	// modify data
+	for (int i=0; i<xfilter_count; i++){
+		if (evt.funcs[i].filter != -1){
+			kevts[nums++] = buildkevent(fd, evt.funcs[i].filter, xaction_del);
+			evt.funcs[i].reset();
+		}
+	}
+	evt.fd = -1;
+	// remove event
+    epoll_event epollevt = buildkevent(evt);
+	epoll_ctl(_epfd, EPOLL_CTL_DEL, fd, &epollevt);
+	_fdnums -= nums;
+	LOG_R("unregevent-fd: fd-%d, nums-%d, left-%d", fd, nums, _fdnums);
+	return 0;
+}
+int call_event_func(struct epoll_event& evt)
+{
+	int fd = evt.data.fd;
+	if (fd == -1) 
+		return -1;
+    xevent& xevt = xeventpool()[fd];
+	if (fd != xevt.fd)
+		return -1;
+	for (int i=0; i<xfilter_count; i++){
+		if (xevt.funcs[i].filter == -1)
+			continue;
+		int kfilter = xfilter2kfilter(xevt.funcs[i].filter);
+		if (kfilter & evt.event){
+			if (xevt.funcs[i].func == NULL) 
+				continue;
+			int rt = func(fd, xevt.funcs[i].filter);
+			if (rt < 0) 
+				return rt;
+		}
+	}
+	return 0;
+}
+struct epoll_event _events[MAX_EVENT_RECV];
+// 0-succ, -1-failed
+int dispatchxevent(int timeoutsecond)
+{
+	int nfds = epoll_wait(_epfd, _events, MAX_EVENT_RECV, timeoutsecond);
+	if (nfds < 0){
+		LOG_E("epoll wait error[%d], ignored!", errno);
+		return -1;
+	}
+	for (int i=0; i<nfds; i++){
+		call_event_func(_events[i]);
+	}
+	return 0;
+};
+
